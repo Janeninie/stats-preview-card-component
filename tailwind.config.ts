506681256d33d8d460c3b905e48cd4cd5c050588
Jane <apiwatsupaphol@gmail.com>
2024-12-15 17:55:06 +0700

    feat: added stats page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,22 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: {
+          veryDarkBlue: "hsl(233, 47%, 7%)",
+          darkDesaturatedBlue: "hsl(244, 38%, 16%)",
+          softViolet: "hsl(277, 64%, 61%)",
+        },
+        second: {
+          white: "hsl(0, 0%, 100%)",
+          slightlyTransparentWhite: {
+            100: "hsla(0, 0%, 100%, 0.75)",
+            200: "hsla(0, 0%, 100%, 0.6)",
+          },
+        },
+      },
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        lexend: ["Lexend Deca", "sans-serif"],
       },
     },
   },
